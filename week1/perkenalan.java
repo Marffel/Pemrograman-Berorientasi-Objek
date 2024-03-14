60cf61951e54ad574cc99bd778899dae28adf403
Muhammad Arif Aldafa <162671223+Marffel@users.noreply.github.com>
2024-03-14 11:29:47 +0700

    Update perkenalan.java

--- week1/perkenalan.java
+++ week1/perkenalan.java
@@ -3,7 +3,7 @@ package week1;
 public class perkenalan {
 
 	public static void main(String[] args) {
-		System.out.println("Halo, Nama Saya Adalah Naufal Arsyaputra Pradana");
+		System.out.println("Halo, Nama Saya Adalah Muhammad Arif Aldafa");
 		System.out.println("Saya Adalah Mahasiswa Teknik Informatika Universitas Dian Nuswantoro");
 		System.out.println("Saya Sedang Menulis Program Java");
 
